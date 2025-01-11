cfa7dc386feca41453aac8d3824ed9f1d54a6239
WalidAra <arawalid90@gmail.com>
2025-01-11 18:40:53 +0100

    fixed

--- client/src/components/layouts/AuthLayout.tsx
+++ client/src/components/layouts/AuthLayout.tsx
@@ -14,7 +14,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
             <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
               <GalleryVerticalEnd className="size-4" />
             </div>
-            Acme Inc.
+            Sprint Vision.
           </a>
           {children}
         </div>
