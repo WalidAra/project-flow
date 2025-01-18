69f39eba9e63ec4e113b9a6a0786bd6fedfa6f53
WalidAra <arawalid90@gmail.com>
2025-01-18 22:08:59 +0100

    fixed bug

--- client/src/components/utils/HomeMiddleware.tsx
+++ client/src/components/utils/HomeMiddleware.tsx
@@ -1,12 +1,17 @@
 import useAuth from "@/hooks/useAuth";
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const HomeMiddleware = ({ children }: { children: React.ReactNode }) => {
   const { isAuthenticated } = useAuth();
+  const location = useLocation();
 
   if (!isAuthenticated) {
-    return <Navigate replace to={"/auth/signin"} />;
+    return <Navigate replace to="/auth/signin" />;
+  }
+
+  if (isAuthenticated && location.pathname === "/") {
+    return <Navigate replace to="/dashboard" />;
   }
 
   return <>{children}</>;
